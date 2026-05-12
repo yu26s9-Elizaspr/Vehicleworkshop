82ff79041cbae89cc7e7a8f61385e01606de7129
elizabeth spratt <espratt@my.yearupunited.org>
2026-05-12 13:17:14 -0400

    completed vehicle class phase 2

--- vehicleProject/src/vehicle.java
+++ vehicleProject/src/vehicle.java
@@ -2,19 +2,80 @@ public class vehicle {
     private int vin;
     private int year;
     private String make;
+    private String model;
     private String vehicleType;
     private String color;
     private int odometer;
     private double price;
 
-    public vehicle (int vin, int year, String make, String vehicleType, String color,
+
+    // Constructor
+    public vehicle (int vin, int year, String make, String model, String vehicleType, String color,
     int odometer, double price) {
         this.vin = vin;
         this.year = year;
         this.make = make;
+        this.model = model;
         this.vehicleType = vehicleType;
         this.color = color;
         this.odometer = odometer;
         this.price = price;
     }
+
+    // Getters
+    public int getVin() {
+        return vin;
+    }
+    public int getYear() {
+        return year;
+    }
+    public String getMake() {
+        return make;
+    }
+    public  String getModel() {
+        return model;
+    }
+    public String getVehicleType() {
+        return vehicleType;
+    }
+    public String getColor() {
+        return color;
+    }
+    public int getOdometer() {
+        return odometer;
+    }
+    public double getPrice() {
+        return price;
+    }
+
+    // Setters
+
+    public void setVin(int vin) {
+        this.vin = vin;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public void setMake(String make) {
+        this.make = make;
+    }
+    public void setModel (String model) {
+        this.model = model;
+    }
+    public void setVehicleType (String vehicleType) {
+        this.vehicleType = vehicleType;
+    }
+    public void setColor (String color) {
+        this.color = color;
+    }
+    public void setOdometer (int odometer) {
+        this.odometer = odometer;
+    }
+    public void setPrice (double price) {
+        this.price = price;
+    }
+
+
 }
